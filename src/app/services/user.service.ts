dd46fd69b0b12caf993a7ba88985bc3296d813b2
Ievgenii Krasnikov <krasenzhenya@gmail.com>
2019-01-29 11:09:40 +0200

    add API

--- src/app/services/user.service.ts
+++ src/app/services/user.service.ts
@@ -1,54 +1,16 @@
 import { Injectable } from '@angular/core';
 
-import { Subject } from 'rxjs';
+import { HttpClient } from '@angular/common/http';
 
-@Injectable({
-  providedIn: 'root'
-})
+@Injectable()
+
 export class UserService {
 
-  public get users$() {
-    return this._users$$.asObservable();
-  }
+  public constructor(
+    private _httpClient: HttpClient,
+  ) {}
 
-  private _users$$ = new Subject();
-
-  private _defaultUsers = [
-    {
-      name: 'Ivan',
-      surname: 'Ivanov',
-      age: '30',
-      email: 'ivanov@mail.ru'
-    },
-    {
-      name: 'Petr',
-      surname: 'Petrov',
-      age: '28',
-      email: 'petrov90@gmail.com'
-    },
-    {
-      name: 'Alisa',
-      surname: 'Krasivaya',
-      age: '22',
-      email: 'beauty123@gmail.com'
-    },
-  ];
-
-  public initData() {
-    this._users$$.next(this._defaultUsers);
-  }
-
-  public setUsers(users) {
-    this._users$$.next(users);
-  }
-
-  public addUser(user) {
-    this._defaultUsers.push(user);
-    this.setUsers(this._defaultUsers);
-  }
-
-  public removeUser(email) {
-    this._defaultUsers = this._defaultUsers.filter((user) => user.email !== email);
-    this.setUsers(this._defaultUsers);
+  public getUsers() {
+    return this._httpClient.get('https://next.json-generator.com/api/json/get/E1HDvnx1I');
   }
 }
--- src/app/table/table.component.ts
+++ src/app/table/table.component.ts
@@ -1,4 +1,7 @@
-import { Component } from '@angular/core';
+import { Component, OnDestroy } from '@angular/core';
+
+import { takeUntil } from 'rxjs/operators';
+import { Subject } from 'rxjs';
 
 import { UserService } from '../services/user.service';
 
@@ -7,16 +10,27 @@ import { UserService } from '../services/user.service';
   templateUrl: './table.component.html',
   styleUrls: ['./table.component.css']
 })
-export class TableComponent {
+export class TableComponent implements OnDestroy {
 
   public users = [];
+
+
+  private _destroy$$ = new Subject();
+
 
   constructor(
     private _userService: UserService,
   ) {
-    this._userService.users$
+    this._userService.getUsers()
+      .pipe(
+        takeUntil(this._destroy$$)
+      )
       .subscribe((users: TUser[]) => {
         this.users = users;
       });
+  }
+
+  public ngOnDestroy(): void {
+    this._destroy$$.next();
   }
 }
